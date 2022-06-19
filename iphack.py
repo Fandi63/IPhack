4fee563a69fcee2523b2301f9ea34407adf6fa87
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2022-06-19 19:20:01 +0200

    Create iphack.py

---
+++ iphack.py
@@ -0,0 +1,11 @@
+import os
+os.system("pkg install toilet")
+os.system("toilet -f mono12 -F gay IPhack")
+print("1:>Ping russia.com")
+print("2:>Ping www.news.ru")
+x=input("Enter option:>")
+if x == 1:
+	os.system("ping russia.com")
+if x == 2:
+	os.system("news.ru")
+
