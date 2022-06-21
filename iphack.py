b12410e884c35caaebafadbf76c7ecee6cf33952
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2022-06-21 18:02:43 +0200

    Update iphack.py

--- iphack.py
+++ iphack.py
@@ -1,15 +1,15 @@
 import os
 os.system("pkg install toilet")
 print("by Fandi63")
-print("=============")
+print("===========================")
 os.system("toilet -f mono12 -F gay IPhack")
-print("=============") 
+print("===========================") 
 print("1:>Ping russia.com")
 print("2:>Ping www.news.ru")
-x=input("Enter option:>")
-if x == 1:
+input("Enter option:>")
+if input == 1:
 	os.system("ping russia.com")
-if x == 2:
+if input == 2:
 	os.system("news.ru")
 
 
